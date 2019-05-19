37d1facc864b0a147cc80d6a211ac33714e4b5ab
User <User@DESKTOP-N7D1EHF>
2019-05-19 08:51:13 +0800

    one

---
+++ readme.txt
@@ -0,0 +1,28 @@
+1.
+http://localhost:8280
+admin
+123
+
+2.
+配置jdk
+配置workspace字符集UTF-8
+配置tomcat
+
+3.
+new-maven project
+next-选中maven-archetype-webapp
+填写
+com.hxzy
+JXC
+-finish
+
+4.jsp报错
+JXC-配置构建路径-Libraies-Add Libray-Server Runtime-next-Finish
+
+5.pom.xml配置
+
+6.
+工具：eclipse + mysql + JDK + tomcat
+技术：SpringBoot+Spring+SpringMvc+Spring Data Jpa+Shiro安全认证+完整权限系统+easyui
+
+
---
+++ src/main/java/com/mf/ServletInitializer.java
@@ -0,0 +1,17 @@
+package com.mf;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+/**
+ * 若打包成war包,则需要继承 org.springframework.boot.context.web.SpringBootServletInitializer类,覆盖其config(SpringApplicationBuilder)方法
+ * @author User
+ *
+ */
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(JxcApplication.class);
+	}
+
+}
